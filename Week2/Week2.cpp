7a7502c1b61825d1f3d1fe421569d2eeb567b1a9
JonathanBlackham <jlbjlb82@gmail.com>
2025-04-11 03:23:03 +0100

    Update Week2.cpp

--- Week2/Week2.cpp
+++ Week2/Week2.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main()
 {
  // dataType variableName;
@@ -12,9 +14,28 @@ int main()
 	double distance;
 	bool isAlive;
 	char initial;
-	std::string name;
+	string name;
 
-	unsigned 
+	unsigned short int betterAge;
+	long double hugeDistance;
+
+	age = 30;
+	weight = 190.5f;
+	distance = 1212.987;
+	isAlive = true;
+	initial = 'j';
+	name = "jonathan";
+
+	cout << "My name is " << name << " and I am " << age << " years old. " << endl;
+	cout << "Am I alive? " << isAlive << endl;
+
+	int newAge;
+	newAge = age; // 30
+
+	int newestAge = 25;
+
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
