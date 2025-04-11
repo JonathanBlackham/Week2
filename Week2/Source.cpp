c0c7c4f83f24a3999b573f47ae4ce96c2f83b494
JonathanBlackham <jlbjlb82@gmail.com>
2025-04-11 08:14:14 +0100

    lessons week2

---
+++ Week2/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+ 
+int ()
+{
+
+	int a = 10;
+	int b = 3;
+
+	int add = a + b; // 13
+	int subtract = 10 - b; //7
+    int multiply = a * 3; // 30
+	int devision = a / b; // 3 or 3.3333
+	int mod = a % b; // 1
+	float div2 = 10 / 3.0f; // 3.3333
+
+	cout << div2 << endl;
+}
